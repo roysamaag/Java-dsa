c1860f0eac4521795fd10f6eb26d386b08219340
Roy <roysam@outlook.com>
2025-08-12 09:28:32 -0500

    max xonsecutive ones

--- TwoPointers.java
+++ TwoPointers.java
@@ -238,4 +238,13 @@ public class TwoPointers {
         System.out.println(ans);
 
     }
+
+    // Given a binary array nums and an integer k, return the maximum number 
+    // of consecutive 1's in the array if you can flip at most k 0's.
+    // Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+    // Output: 6
+    // Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+
+    
+
 }
