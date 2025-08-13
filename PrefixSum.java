52648144833f0dd3ac037c3b176b61996dc37745
Roy Sam <rsam@starbucks.com>
2025-08-12 18:39:32 -0700

    prefix sum

---
+++ PrefixSum.java
@@ -0,0 +1,28 @@
+public class PrefixSum{
+    
+    public static void main(String[] args) {
+        int[] nums = {1, 6, 3, 2, 7, 2};
+        int[][] queries = {{3,0}, {4,2}};
+        StorePrefixSums(nums,queries);
+
+        
+    }
+
+    // nums = [1, 6, 3, 2, 7, 2]
+    public static void StorePrefixSums(int[] nums, int[][] queries){
+        int[] prefixes = new int[nums.length];
+        
+        prefixes[0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++){
+            prefixes[i] = nums[i] + prefixes[i-1];
+        }
+
+        for (int i = 0; i < prefixes.length; i++){
+            System.out.println(prefixes[i]);
+        }
+
+    }
+
+
+}
